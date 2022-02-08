d6bc606f908a8eeaf7fbb26b2ae35ed5c8b4e780
Mat Cameron <cameron.mathieu@student.greenriver.edu>
2022-02-08 18:19:26 -0500

    added model folder

---
+++ model/data-layer.php
@@ -0,0 +1,8 @@
+<?php
+function getCondiments(){
+    return array('mayo','mustard','ketchup','salsa','bbq','hot sauce');
+}
+
+function getBeverage(){
+    return array('pepsi','coke','mt dew','rootbeer');
+}
